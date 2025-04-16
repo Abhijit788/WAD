89c8e7f1e57a1c8d58f3d9542c84d56ebeedf592
Abhijit Arote <109600974+Abhijit788@users.noreply.github.com>
2025-04-16 18:18:46 +0000

    as

---
+++ client/src/store/index.js
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import auth from './authSlice';
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+const persistConfig ={
+    key: 'Tarnag',
+    storage
+}
+
+const persistAuthReducer = persistReducer(persistConfig, auth)
+
+export const store = configureStore({
+    reducer: {
+        auth: persistAuthReducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false,
+      }),
+});
